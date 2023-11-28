e5c3da06428451949bb5d62c78d3cf39176506ee
Mahima0Verma <97933817+Mahima0Verma@users.noreply.github.com>
2023-11-28 23:10:26 +0530

    Add files via upload

---
+++ Kalam/login.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+    <title>Login Form</title>
+  </head>
+  <body>
+    <div class="container mt-5 g-3 p-3 mx-auto pt-3" style="background-color: rgb(168, 196, 168); width: 800px; height: 500px;">
+    <form>
+        <!-- Email input -->
+        <div data-mdb-input-init class="form-outline m-4 m-auto" style="width: 300px ; margin-top:120px !important; ">
+          <input type="text" id="form1Example1" class="form-control" placeholder="Username" />
+          <label class="form-label" for="form1Example1"></label>
+        </div>
+      
+        <!-- Password input -->
+        <div data-mdb-input-init class="form-outline m-4 m-auto" style="width: 300px; margin-top: 20px;">
+          <input type="password" id="form1Example2" class="form-control" placeholder="Password" />
+          <label class="form-label" for="form1Example2"></label>
+           <!-- Submit button -->
+        <button data-mdb-ripple-init type="submit" class="btn btn-success btn-block mt-4" style="background-color: green;" >Register</button>
+
+         <!-- Submit button -->
+         <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block mt-4" style="margin-left: 80px;">Signup</button>
+        </div>
+      
+        <!-- 2 column grid layout for inline styling -->
+        
+       
+      </form>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
+
+
+
+
+
+
+
